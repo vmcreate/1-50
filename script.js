30b87b430859cf228cd45227bad9498e3d7187f2
Viktor <29626952+vmcreate@users.noreply.github.com>
2020-12-02 23:07:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeClass();
+        panel.classList.add('active');
+    })
+})
+
+
+function removeClass() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
